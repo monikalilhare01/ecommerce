999598d4929707d9b2def3f0eb699ec760d32b4a
unknown <monikalilharekv@gmail.com>
2025-09-09 12:52:33 +0530

    function definition included in token file

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,9 +1,31 @@
 package tokens
 
-func TokenGenerator() {
+type SignedDetails struct {
+	Email      string
+	First_Name string
+	Last_Name  string
+	Uid
+	jwt.StandardClaims
+}
 
+func TokenGenerator(email, first_name, last_name, uid string) (signedtoken, signedrefreshtoken string, err error) {
+	claims := &SignedDetails{
+		Email:email,
+		First_Name: first_name,
+		Last_Name: last_name,
+		Uid:uid,
+		StandardClaims:jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour*time.Duration(24)).Unix(),
+		} 
+	}
+
+	
 }
 
 func UpdateAllTokens() {
+
+}
+
+func ValidateToken() {
 
 }
